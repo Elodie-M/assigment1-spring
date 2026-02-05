e9fc24dcc5a6fcc40067653c7e7333f825b87cb0
Elodie Mesiha <mesiha@sheridancollege.ca>
2026-02-04 23:02:26 -0500

    initial commit

---
+++ src/test/java/com/example/Assigment1/Assigment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assigment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assigment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
