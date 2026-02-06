bf0426ccf003e713bcfd85a5cce045d374ab87e4
Elodie Mesiha <mesiha@sheridancollege.ca>
2026-02-06 13:13:40 -0500

    Fixed form binding and home route

--- src/main/java/com/example/Assigment1/controller/HomeController.java
+++ src/main/java/com/example/Assigment1/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-         return "order_form";
+         return "redirect:/order";
     }
 }
--- src/main/java/com/example/Assigment1/controller/PizzaOrderController.java
+++ src/main/java/com/example/Assigment1/controller/PizzaOrderController.java
@@ -1,10 +1,14 @@
 package com.example.Assigment1.controller;
 
+import com.example.Assigment1.model.CrustType;
 import com.example.Assigment1.model.PizzaOrder;
+import com.example.Assigment1.model.PizzaSize;
+import com.example.Assigment1.model.Topping;
 import com.example.Assigment1.service.PizzaOrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,9 +20,16 @@ public class PizzaOrderController {
         this.service = service;
     }
 
+    @ModelAttribute("pizzaOrder")
+    public PizzaOrder pizzaOrder() {
+        return new PizzaOrder();
+    }
+
     @GetMapping("/order")
-    public String order(Model model) {
-        model.addAttribute("pizzaOrder", new PizzaOrder());
+    public String order() {
+        //model.addAttribute("sizes", PizzaSize.values());
+        //model.addAttribute("crusts", CrustType.values());
+        //model.addAttribute("toppings", Topping.values());
         return "order_form";
     }
 
