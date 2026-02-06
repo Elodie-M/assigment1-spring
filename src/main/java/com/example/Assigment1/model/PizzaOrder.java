d7a44cb42a9285140d0d887d0489a3933c8d9af1
Elodie Mesiha <mesiha@sheridancollege.ca>
2026-02-05 21:38:28 -0500

    created the PizzaOrder class

---
+++ src/main/java/com/example/Assigment1/model/PizzaOrder.java
@@ -0,0 +1,118 @@
+package com.example.Assigment1.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PizzaOrder {
+
+    private long id;
+    private String customerName;
+
+    private PizzaSize size;
+    private CrustType crust;
+    private List<Topping> toppings;
+
+    private int quantity;
+    private boolean delivery;
+    private String deliveryAddress;
+
+    private double subtotal;
+    private double discountAmount;
+    private double tax;
+    private double total;
+
+    private LocalDateTime orderTime;
+
+    // Getter/Setter
+
+    public long getID(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getCustomerName(){
+        return customerName;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+
+    public PizzaSize setsSize(){
+        return size;
+    }
+    public void setSize(PizzaSize size){
+        this.size = size;
+    }
+
+    public CrustType getCrust(){
+        return crust;
+    }
+    public void setCrust(CrustType crust){
+        this.crust = crust;
+    }
+
+    public List<Topping> getToppings(){
+        return toppings;
+    }
+    public void setToppings(List<Topping> toppings){
+        this.toppings = toppings;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public boolean isDelivery(){
+        return delivery;
+    }
+    public void setDelivery(boolean delivery){
+        this.delivery = delivery;
+    }
+
+    public String getDeliveryAddress(){
+        return deliveryAddress;
+    }
+    public void setDeliveryAddress(String deliveryAddress){
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public double getSubtotal(){
+        return subtotal;
+    }
+    public void setSubtotal(double subtotal){
+        this.subtotal = subtotal;
+    }
+
+    public double getDiscountAmount(){
+        return discountAmount;
+    }
+    public void setDiscountAmount(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getTax(){
+        return tax;
+    }
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+}
